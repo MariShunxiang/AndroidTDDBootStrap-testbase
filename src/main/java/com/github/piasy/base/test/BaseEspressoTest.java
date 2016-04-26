ed2c83a5da325b64caffb1346525198ff264ecb0
Piasy <xz4215@gmail.com>
2016-04-26 21:35:00 +0800

    import RESTMock

--- src/main/java/com/github/piasy/base/test/BaseEspressoTest.java
+++ src/main/java/com/github/piasy/base/test/BaseEspressoTest.java
@@ -25,10 +25,6 @@
 package com.github.piasy.base.test;
 
 import java.io.IOException;
-import okhttp3.mockwebserver.Dispatcher;
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.After;
 import org.junit.Before;
 
@@ -37,29 +33,12 @@ import org.junit.Before;
  */
 public class BaseEspressoTest {
 
-    public static final int MOCK_WEB_SERVER_PORT = 9876;
-
-    private MockWebServer mMockWebServer;
-
     @Before
     public void setUp() throws IOException {
-        mMockWebServer = new MockWebServer();
-        mMockWebServer.start(MOCK_WEB_SERVER_PORT);
-        mMockWebServer.setDispatcher(createDispatcher());
     }
 
     @After
     public void tearDown() throws IOException {
-        mMockWebServer.shutdown();
-    }
-
-    protected Dispatcher createDispatcher() {
-        return new Dispatcher() {
-            @Override
-            public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
-                return new MockResponse();
-            }
-        };
     }
 
     protected void sleep(int millis) {
